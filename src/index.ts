2afa9717514c67b777114f2c61f59a8d46cc4741
mehul79 <mehulrgupta@gmail.com>
2025-02-22 11:58:45 +0530

    refactor routing to redirect all unmatched paths to /todos

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ import {get_todos} from "./getTodos.js"
 
 const app = new Hono()
 
-app.get("*", async(c)=>{
-  return c.redirect("/todos")
-})
-
 app.get('/todos', async(c) => {
   const imported_array = get_todos()
   console.log(imported_array);
   return c.json(imported_array)
+})
+
+app.get("/*", async(c)=>{
+  return c.redirect("/todos")
 })
 
 export default app
